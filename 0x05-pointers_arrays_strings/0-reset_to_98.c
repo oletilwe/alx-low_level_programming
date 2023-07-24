c4d7821c9aefe6c8a8b4360bd4953954fc413918
oletilwe <oletilwelephalala8@gmail.com>
2023-07-24 04:28:03 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* main - value of pointer and updates it to 98
+* Return: always 0
+*/
+
+void reset_to_98(int *n)
+{
+if (n != NULL)
+{
+*n = 98;
+}
+}
