e70e8da6337dad382398924f3d467a66060cb2de
oletilwe <oletilwelephalala8@gmail.com>
2023-08-16 09:11:55 -0700

    0. What's my name, 1. If you spend too much time thinking about a thing, you'll never get it done)

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_name - a function that prints a name.
+* @name: string to add
+* @f: pointer to a function
+* Return: nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
