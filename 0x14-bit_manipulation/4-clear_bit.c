590cfe4036c6c97fd8e3e3d0540ced88fdfa262e
oletilwe <oletilwelephalala8@gmail.com>
2023-09-28 06:05:15 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,12 +11,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask;
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index > 63)
 return (-1);
-}
-mask = 1UL << index;
-*n &= ~mask;
-return (-1);
+*n = (~(1UL << index) & *n);
+return (1);
 }
