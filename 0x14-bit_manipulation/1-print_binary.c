98597c5b631263cf74b6d4d5cd190717450a19f7
oletilwe <oletilwelephalala8@gmail.com>
2023-09-28 06:01:50 -0700

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,19 @@
 
 void print_binary(unsigned long int n)
 {
-int shift = sizeof(unsigned long int) * 8 - 1;
-int flag = 0;
-while (shift >= 0)
+int i, count = 0;
+unsigned long int current;
+for (i = 63; i >= 0; i--)
 {
-unsigned long int bit = (n >> shift) & 1;
-if (bit == 1)
-flag = 1;
-if (flag)
-(bit == 1) ? write(1, "1", 1) : write(1, "0", 1);
-shift--;
+current = n >> i;
+if (current & 1)
+{
+_putchar('1');
+count++;
 }
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
 }
